ecd436b65f1f31664756e2d21459e4304feca763
aripos1 <ihsjjang@gmail.com>
2024-07-01 17:49:55 +0900

    수정

--- src/com/javaex/ex04/PointDraw.java
+++ src/com/javaex/ex04/PointDraw.java
@@ -4,18 +4,18 @@ public class PointDraw {
 
 	public static void main(String[] args) {
 		
-		Point p1 = new Point();
-		Point p2 = new Point();
+		Point point1 = new Point();
+		Point point2 = new Point();
 		
 		
-		p1.setX(3);
-		p1.setY(5);
+		point1.setX(3);
+		point1.setY(5);
 		
-		p2.setX(10);
-		p2.setY(23);
+		point2.setX(10);
+		point2.setY(23);
 		
-		p1.draw();
-		p2.draw();
+		point1.draw();
+		point2.draw();
 	}
 
 }
