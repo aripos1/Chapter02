00dcae8bfc4c06fa37deeb1740075d60c7ceb4e5
aripos1 <ihsjjang@gmail.com>
2024-07-02 11:18:54 +0900

    .

--- src/com/javaex/ex05/Song.java
+++ src/com/javaex/ex05/Song.java
@@ -9,48 +9,57 @@ public class Song {
 	private int year;
 	private int track;
 
-	public void setTitle(String a) {
-		title = a;
+//	public Song (String title,String artist,String album,String composer,int year,int track) {
+//		this.title = title;
+//		this.artist = artist;
+//		this.album = album;
+//		this.composer = composer;
+//		this.year = year;
+//		this.track = track;
+//	}
+//		
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
-	public void setArtist(String b) {
-		artist = b;
+	public void setArtist(String artist) {
+		this.artist = artist;
 	}
 
 	public String getArtist() {
 		return artist;
 	}
 
-	public void setAlbum(String c) {
-		album = c;
+	public void setAlbum(String album) {
+		this.album = album;
 	}
 
 	public String getAlbum() {
 		return album;
 	}
 
-	public void setComposer(String d) {
-		composer = d;
+	public void setComposer(String composer) {
+		this.composer = composer;
 	}
 
 	public String getComposer() {
 		return composer;
 	}
 
-	public void setYear(int e) {
-		year = e;
+	public void setYear(int year) {
+		this.year = year;
 	}
 
 	public int getYear() {
 		return year;
 	}
 
-	public void setTrack(int f) {
-		track = f;
+	public void setTrack(int track) {
+		this.track = track;
 	}
 
 	public int getTrack() {
