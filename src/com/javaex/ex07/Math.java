944796a92f9d6533464bc8ce83f4440d54788297
aripos1 <ihsjjang@gmail.com>
2024-07-03 13:08:04 +0900

    .

---
+++ src/com/javaex/ex07/Math.java
@@ -0,0 +1,32 @@
+package com.javaex.ex07;
+
+public class Math {
+
+	public int plus(int a, int b) {
+
+		int sum = a + b;
+		return sum;
+
+	}
+
+	public double plus(double a, int b) {
+
+		double sum = a + b;
+		return sum;
+
+	}
+
+	public double plus(int a, double b) {
+
+		double sum = a + b;
+		return sum;
+
+	}
+	
+	public double plus(double a, double b) {
+
+		double sum = a + b;
+		return sum;
+
+	}
+}
---
+++ src/com/javaex/ex07/MathApp.java
@@ -0,0 +1,11 @@
+package com.javaex.ex07;
+
+public class MathApp {
+
+	public static void main(String[] args) {
+		
+		Math math = new Math();
+		System.out.println(math.plus(3, 3.5));
+		System.out.println(math.plus(3, 3));
+	}
+}
